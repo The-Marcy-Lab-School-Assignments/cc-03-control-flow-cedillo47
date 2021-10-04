fdefd009adf11654c3cfaab682c19da12aa9ac5c
leo <cedillo47@gmail.com>
2021-10-04 14:22:53 +0000

    questions attempted 1,2,3,5

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
 //1
-function countToTen(){
+function countFromOne(num){ 
+    for(let i = 1; i<= num; i++){
+        console.log(i)
+    }
   
 }
 
 //2
-function countFromOne(){
-  
+function isNegative(num){
+ let number = false
+ if(Math.sign(num) === 1){
+     console.log(number)
+ } else {
+     number = true;
+     console.log(number)
+ }
 }
 
+isNegative(-1)
 //3
 function countEveryEven(){
   
 }
 
 //4
-function countEveryOdd(){
+function countEveryOdd(x){
+    for(let i = 1; i < x; i++){
+        if ( i % 3 === 0){
+            console.log(i)
+        }
+    }
   
 }
 
 //5 
-function countEvens(){
-  
+function isAllLowerCase(string){
+  if (string === string.toLowerCase()){
+      return true 
+  } else {
+      return false
+  }
 }
 
+console.log(isAllLowerCase('Hello'))
